cff18a1fe7182f856d0b665c68636a48f25b537d
Rediet Ayele <reayele@gmail.com>
2026-06-30 17:29:15 -0400

    Complete Player class

--- player.py
+++ player.py
@@ -8,10 +8,21 @@ Date: 06/27/26
 from coin import Coin
 
 class Player:
-    def __init__(self, name):
-        self.__name = name
-        self.__wallet = 20
-        self.__coin = Coin()
-
-
+        def __init__(self, name):
+            self.__name = name
+            self.__wallet = 20
+            self.__coin = Coin()
+       
+        def get_name(self):
+            return self.__name
+        def get_wallet(self):
+            return self.__wallet
+        def win_coin(self):
+            self.__wallet += 1
+        def lose_coin(self):
+             self.__wallet -= 1
+        def toss_coin(self):
+             self.__coin.toss()
+        def get_coin_side(self):
+             return self.__coin.get_sideup()
 
