31a02e86247239a37121e42f4d232f8d1e64d5de
Rediet Ayele <reayele@gmail.com>
2026-06-30 16:13:47 -0400

    Implement Coin class

--- coin.py
+++ coin.py
@@ -4,3 +4,17 @@ Author: Rediet Ayele
 Purpose: This class represents a single, tossable coin. It only knows about its own state (heads or tails).
 Date: 06/27/26
 """
+
+import random
+
+class Coin:
+    def __init__(self):
+        self.__sideup = "Heads"
+
+    def toss(self):
+        random_number = random.randint(0, 1)
+
+        if random_number == 0:
+            self.__sideup = "Heads"
+        else:
+            self.__sideup = "Tails"
