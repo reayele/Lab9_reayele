7bbe8575509cfd8cd871efaf870fd323178e6717
Rediet Ayele <reayele@gmail.com>
2026-06-30 16:42:22 -0400

    Create Player class and attributes

--- player.py
+++ player.py
@@ -4,3 +4,14 @@ Author: Rediet Ayele
 Purpose: This class represents a player. A player has a name, has a wallet of coins, and has a Coin object to toss.
 Date: 06/27/26
 """
+
+from coin import Coin
+
+class Player:
+    def __init__(self, name):
+        self.__name = name
+        self.__wallet = 20
+        self.__coin = Coin()
+
+
+
