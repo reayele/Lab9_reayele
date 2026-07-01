811b317e7a63e6efc3699dc09b5e3495681fda20
Rediet Ayele <reayele@gmail.com>
2026-06-30 23:37:56 -0400

    Added score display

--- Lab9_reayele-1.py
+++ Lab9_reayele-1.py
@@ -19,35 +19,47 @@ def main():
     coins = input("Do you want to toss the coins? (y/n): ")
 
     while coins == "y":
-        print("Tossing...")
+     print("Tossing...")
 
-        player1.toss_coin()
-        player2.toss_coin()
-        side1 = player1.get_coin_side()
-        side2 = player2.get_coin_side()
-        wallet1 = player1.get_wallet()
-        wallet2 = player2.get_wallet()
+    player1.toss_coin()
+    player2.toss_coin()
 
-        print(player1.get_name(), "tossed", side1)
-        print(player2.get_name(), "tossed", side2)
+    side1 = player1.get_coin_side()
+    side2 = player2.get_coin_side()
 
-        if side1 == side2:
-            print("...Match! Player 1 wins a coin.")
-            player1.win_coin()
-            player2.lose_coin()
-            print(player1.get_name(), "has", wallet1)
-            print(player2.get_name(), "has", wallet2)
+    print(player1.get_name(), "tossed", side1)
+    print(player2.get_name(), "tossed", side2)
 
-        else:
-            print("...No Match! Player 2 wins a coin.")
-            player1.lose_coin()
-            player2.win_coin()
-            print(player1.get_name(), "has", wallet1, "coins")
-            print(player2.get_name(), "has", wallet2, "coins")
+    if side1 == side2:
+        print("...Match! Player 1 wins a coin.")
+        player1.win_coin()
+        player2.lose_coin()
+
+    else:
+        print("...No Match! Player 2 wins a coin.")
+        player1.lose_coin()
+        player2.win_coin()
+
+    print(player1.get_name(), "has", player1.get_wallet(), "coins.")
+    print(player2.get_name(), "has", player2.get_wallet(), "coins.")
+
+    coins = input("Do you want to toss the coins? (y/n): ")
+
+    while coins == "n":
+     print("--- Final Score ---")
+    print(player1.get_name(), ":", player1.get_wallet())
+    print(player2.get_name(), ":", player2.get_wallet())
+
+    if player1.get_wallet() == player2.get_wallet():
+        print("It's a draw!")
+
+    elif player1.get_wallet() > player2.get_wallet():
+        print(player1.get_name(), "wins!")
+
+    else:
+     print(player2.get_name(), "wins!")
 
 
 
-
-        coins = input("Do you want to toss the coins? (y/n): ")
 if __name__ == "__main__":
     main()
