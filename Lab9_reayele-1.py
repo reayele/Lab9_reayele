711b9f33e8543d793efc42e1e49c17c1e46b80e1
Rediet Ayele <reayele@gmail.com>
2026-06-30 22:34:18 -0400

    Create main setup

--- Lab9_reayele-1.py
+++ Lab9_reayele-1.py
@@ -4,3 +4,21 @@ Author: Rediet Ayele
 Purpose: This file runs the game. It creates the Player objects and manages the game loop and rules.
 Date: 06/27/26
 """
+
+from player import Player
+
+def main():
+    print("--- Coin Match Game ---")
+
+    player1 = Player("Player 1")
+    player2 = Player("Player 2")
+
+    print(player1.get_name(), "has", player1.get_wallet(), "coins.")
+    print(player2.get_name(), "has", player2.get_wallet(), "coins.")
+
+    coins = input(print("Do you want to toss the coins? (y/n): "))
+ 
+if __name__ == "__main__":
+    main()
+
+
